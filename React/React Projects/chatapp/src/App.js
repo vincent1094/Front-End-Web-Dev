c1670675faa0bd90e87ffc896535b9fd91e5401d
Thanh Nguyen <joseph_nguyen1094@yahoo.com>
2024-01-04 17:58:32 +0700

    Chat App Components

---
+++ React/React Projects/chatapp/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { ChatEngine } from "react-chat-engine"
+import ChatFeed from "./components/ChatFeed"
+
+function App() {
+  return (
+    <div className="App">
+      <ChatEngine 
+        height="100vh"
+        projectID="aae4ae99-0933-4730-a8b2-098c8cbc8dcf"
+        userName="thanh"
+        userSecret="nguyen"
+        renderChatFeed= { (chatAppProps) => <ChatFeed  {...chatAppProps} />}
+        // onNewMessage= { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3')}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/React Projects/chatapp/src/components/TheirMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheirMessage = () => {
+    return (
+        <div>
+            My TheirMessage
+        </div>
+    );
+}
+
+export default TheirMessage;
